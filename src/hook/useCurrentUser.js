bd2281ea652570adeb2da2ece4d8d8ddbeb48c65
obb <obb.image@gmail.com>
2024-06-11 14:54:14 +0700

    add set token when set user

--- src/hook/useCurrentUser.js
+++ src/hook/useCurrentUser.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { isObjEmpty } from "../util/object";
 import { getStorageUser, removeStorageTokenUser, removeStorageUser, setStorageUser } from "../util/localStorage";
+import useToken from "./token";
 
 export default function useCurrentUser() {
     const [currentUser, setCurrentUser] = useState({});
+    const { token, setToken } = useToken();
 
     useEffect(() => {
         if (isObjEmpty(currentUser) || currentUser) {
@@ -14,9 +16,15 @@ export default function useCurrentUser() {
         }
     }, [currentUser]);
 
+    const saveCurrentUser = (user, token) => {
+        setCurrentUser(user);
+        if (token) {
+            setToken(token);
+        }
+    }
     return {
         currentUser,
-        setCurrentUser
+        setCurrentUser: saveCurrentUser
     }
 
 }
